729ef8ed8ba4233ca551d6d0b93c0f05c5c57e57
SpaceDad777 <gwenithv@gmail.com>
2023-11-13 17:12:52 -0700

    Update sketch.js

--- Homework-11/sketch.js
+++ Homework-11/sketch.js
@@ -1,131 +1,136 @@
-//X and Y for my character
+// X and Y for my character
 var characterX = 100;
 var characterY = 100;
 
-//key codes for each letter
+// Key codes for each letter
 var w = 87;
 var s = 83;
 var a = 65;
 var d = 68;
 
-//x and y for the object
+// Variables for the first enemy circle
 var shapeX = 30;
 var shapeY = 50;
 var shapeXSpeed;
-var shapeYspeed;
+var shapeYSpeed;
 
-//creating a shape when mouse is clicked
+// Variables for the second enemy circle
+var enemy2X = 150;
+var enemy2Y = 200;
+var enemy2XSpeed;
+var enemy2YSpeed;
+
+// Creating a shape when the mouse is clicked
 var mouseShapeX;
 var mouseShapeY;
 
-function setup()
-{
-    createCanvas(500,600);
-    //random speed when it first starts
-    shapeXSpeed = Math.floor(Math.random() * (Math.floor(Math.random() * 3)) + 1);
-    shapeYSpeed = Math.floor(Math.random() * (Math.floor(Math.random() * 3)) + 1);
-}
+function moveEnemy() {
+    shapeX -= shapeXSpeed;
+    shapeY -= shapeYSpeed;
 
-function draw()
-{
-    background(120,45,78);
-    stroke(0);
-    fill(0); 
-    
-
-    //top
-    rect(0,0,width,10);
-
-    //left
-    rect(0,0,10,height);
-
-    //bottom
-    rect(0,height -10,width, 10);
-
-    //right
-    rect(width-10,0,10,height-50);
-
-    //completion message
-    textSize(20);
-    text("Exit", width-50,height-50)
-
-    //character
-    fill(23,40,123);
-    circle(characterX, characterY,25);
-
-    if(keyIsDown(w))
-    {
-        characterY -=10;
+    // Bounce off the canvas edges for the first enemy circle
+    if (shapeX > width || shapeX < 0) {
+        shapeXSpeed *= -1;
     }
 
-    if(keyIsDown(s))
-    {
+    if (shapeY > height || shapeY < 0) {
+        shapeYSpeed *= -1;
+    }
+}
+
+function moveEnemy2() {
+    // Move the second enemy in the opposite direction
+    enemy2X -= enemy2XSpeed;
+    enemy2Y -= enemy2YSpeed;
+
+    // Bounce off the canvas edges for the second enemy circle
+    if (enemy2X > width - 20 || enemy2X < 20) {
+        enemy2XSpeed *= -1;
+    }
+
+    if (enemy2Y > height - 20 || enemy2Y < 20) {
+        enemy2YSpeed *= -1;
+    }
+}
+
+function setup() {
+    createCanvas(500, 600);
+    // Random speed when it first starts for the first enemy circle
+    shapeXSpeed = Math.floor(Math.random() * (Math.floor(Math.random() * 3)) + 1);
+    shapeYSpeed = Math.floor(Math.random() * (Math.floor(Math.random() * 3)) + 1);
+
+    // Random speed when it first starts for the second enemy circle
+    enemy2XSpeed = Math.floor(Math.random() * (Math.floor(Math.random() * 3)) + 1);
+    enemy2YSpeed = Math.floor(Math.random() * (Math.floor(Math.random() * 3)) + 1);
+}
+
+function draw() {
+    background(0, 0, 255);
+    stroke(0);
+    fill(0);
+
+    // top
+    rect(0, 0, width, 10);
+
+    // left
+    rect(0, 0, 10, height);
+
+    // bottom
+    rect(0, height - 10, width, 10);
+
+    // right
+    rect(width - 10, 0, 10, height - 50);
+
+    // completion message
+    textSize(20);
+    text("Exit", width - 50, height - 50);
+
+    // First Enemy Circle
+    fill(13, 145, 14);
+    circle(shapeX, shapeY, 80);
+
+    // Second Enemy Circle
+    fill(255, 0, 0); // Red color for the second enemy circle
+    circle(enemy2X, enemy2Y, 40);
+
+    // character
+    fill(23, 40, 123);
+    circle(characterX, characterY, 25);
+
+    if (keyIsDown(w)) {
+        characterY -= 10;
+    }
+
+    if (keyIsDown(s)) {
         characterY += 10;
     }
 
-    if(keyIsDown(a))
-    {
+    if (keyIsDown(a)) {
         characterX -= 10;
     }
 
-    if(keyIsDown(d))
-    {
+    if (keyIsDown(d)) {
         characterX += 10;
     }
-    
-    //enemy
-    fill(13,145,14);
-    circle(shapeX,shapeY, 10);
-    circle(x,y,diameter);
-    x += 10;
-    y += 3;
-    
 
-     //random speed when it initally starts
-     shapeXSpeed = Math.floor(Math.random() * (Math.floor(Math.random() * 3)) + 1);
-     shapeYSpeed = Math.floor(Math.random() * (Math.floor(Math.random() * 3)) + 1);
-     
-    //move the shape
-    shapeX += shapeXSpeed;
-    shapeY += shapeYspeed;
+    // Move the shapes
+    moveEnemy();
+    moveEnemy2();
 
-    //check to see if the shape has gone out of bounds
-    if(shapeX > width)
-    {
-        shapeX = 0;
-    }
-
-    if(shapeX < 0)
-    {
-        shapeY = width;
-    }
-
-    if(shapeY > height)
-    {
-        shapeY = 0;
-    }
-
-    if(shapeY < 0)
-    {
-        shapeY =  height;
-    }
-    //character reaches the exit
-    if(characterX > width && characterY > width-50)
-    {
+    // character reaches the exit
+    if (characterX > width && characterY > width - 50) {
         fill(0);
         stroke(5);
         textSize(48);
-        text("You Win!", width/2-50, height/2-50);
+        text("You Win!", width / 2 - 50, height / 2 - 50);
     }
 
-    //creating a shape based on the mouse click
-    fill(120,1030,140);
+    // creating a shape based on the mouse click
+    fill(120, 103, 140);
     circle(mouseShapeX, mouseShapeY, 25);
-
 }
 
-function mouseClicked()
-{
+function mouseClicked() {
     mouseShapeX = mouseX;
     mouseShapeY = mouseY;
 }
